74c106066a769f434567b1da92c7fe789e63f073
pkcheng <pkcheng2000@hotmail.com>
2021-01-07 22:18:28 +0800

    New Code

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <main>
       <section className="container">
         <div className="title">
+          <h2>Practice Project</h2>
           <h2>Our Reviews</h2>
           <div className="underline"></div>
         </div>
